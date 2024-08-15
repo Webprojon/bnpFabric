88f4a008528ed8803d25fd8322acd70c0edb59aa
Webprojon <tokhirjonkhasanov@gmail.com>
2024-08-15 11:03:42 +0200

    collection product's responsive is done

--- src/pages/HomePage/Collection-Product.tsx
+++ src/pages/HomePage/Collection-Product.tsx
@@ -1,6 +1,8 @@
 import { Link, useParams } from "react-router-dom";
 import { products } from "../../lib/data";
 import { useState } from "react";
+import InnerImageZoom from "react-inner-image-zoom";
+import "react-inner-image-zoom/lib/InnerImageZoom/styles.css";
 
 export default function ProductDetails() {
 	const [isActivePage, setIsActivePage] = useState(false);
@@ -39,18 +41,22 @@ export default function ProductDetails() {
 	}
 
 	return (
-		<section className="mx-auto max-w-[450px] xs:max-w-[600px] sm:max-w-[800px] md:max-w-[1460px]">
-			<div className="my-14 flex justify-between items-center">
-				<img
+		<section className="mx-auto px-2 lg:px-0 max-w-[450px] xs:max-w-[600px] sm:max-w-[800px] md:max-w-[1460px]">
+			<div className="my-6 md:my-14 flex flex-col md:flex-row justify-between items-center">
+				<InnerImageZoom
 					src={product.src}
-					alt={product.text}
-					className="rounded-[20px] w-[720px] h-[96vh]"
+					zoomSrc={product.src}
+					className="rounded-[20px] md:w-[520px] md:h-[80vh] lg:w-[720px] lg:h-[96vh]"
+					zoomType="hover"
+					hideHint={true}
 				/>
 
 				<div className="tracking-wide">
-					<h2 className="font-bold text-[40px]">{product.text}</h2>
-					<table className="w-[500px] my-4 border border-black/30 text-black/70">
-						<tbody className="text-[17px]">
+					<h2 className="font-bold mt-8 text-[28px] md:text-[40px]">
+						{product.text}
+					</h2>
+					<table className="lg:w-[460px] my-4 border border-black/30 text-black/70">
+						<tbody className="text-[15px] xs:text-[17px]">
 							<tr className="border-b border-black/30">
 								<td className="px-6 py-4 border-r border-black/30">
 									Material:
@@ -100,9 +106,9 @@ export default function ProductDetails() {
 				</div>
 			</div>
 
-			<div className="border-t border-black/40 my-[6rem]">
+			<div className="border-t border-black/40 my-[3rem] md:my-[6rem]">
 				<div
-					className={`flex justify-center items-center space-x-20 text-[18px] font-bold cursor-pointer 
+					className={`flex justify-center items-center space-x-10 md:space-x-20 md:text-[18px] font-bold cursor-pointer 
 						${isActivePage ? "text-black" : "text-black/60"}`}
 				>
 					<span onClick={defineActivePage} className={tabStyles(isActivePage)}>
@@ -114,23 +120,23 @@ export default function ProductDetails() {
 				</div>
 
 				{isActivePage ? (
-					<div className="mt-20 flex flex-col mx-auto max-w-[1020px] tracking-wide">
+					<div className="mt-8 md:mt-20 flex flex-col mx-auto max-w-[1020px] tracking-wide">
 						<div>
-							<h2 className="font-bold text-[25px]">Reviews</h2>
-							<p className="py-10 text-[17px] text-black/70">
+							<h2 className="font-bold text-[20px] md:text-[25px]">Reviews</h2>
+							<p className="py-5 md:py-10 text-[17px] text-black/70">
 								There are no reviews yet
 							</p>
-							<p className="font-bold text-[24px]">
+							<p className="font-bold text-[18px] md:text-[24px]">
 								Be the first to review “{product.text}”
 							</p>
-							<p className="text-black/85 tracking-wider mb-10">
+							<p className="text-black/85 tracking-wider mt-2 mb-6 md:mb-10">
 								Your email address will not be published. Required fields are
-								marked *
+								marked
 							</p>
 						</div>
 
 						<form className="flex flex-col space-y-7">
-							<div className="flex justify-between">
+							<div className="flex flex-col md:flex-row justify-between">
 								<div className="flex flex-col">
 									<label
 										htmlFor="name"
@@ -143,10 +149,10 @@ export default function ProductDetails() {
 										required
 										id="name"
 										autoComplete="off"
-										className="bg-gray-200 rounded-[27px] w-[470px] py-4 px-6 outline-none"
+										className="bg-gray-200 rounded-[22px] md:rounded-[27px] md:w-[470px] py-3 md:py-4 px-6 outline-none"
 									/>
 								</div>
-								<div className="flex flex-col">
+								<div className="flex flex-col mt-4 sm:mt-0">
 									<label
 										htmlFor="email"
 										className="text-[18px] mb-3 text-black/70"
@@ -158,7 +164,7 @@ export default function ProductDetails() {
 										required
 										id="email"
 										autoComplete="off"
-										className="bg-gray-200 rounded-[27px] w-[470px] py-4 px-6 outline-none"
+										className="bg-gray-200 rounded-[22px] md:rounded-[27px] md:w-[470px] py-3 md:py-4 px-6 outline-none"
 									/>
 								</div>
 							</div>
@@ -172,22 +178,23 @@ export default function ProductDetails() {
 								</label>
 								<textarea
 									id="review"
-									rows={9}
 									required
 									autoComplete="off"
-									className="bg-gray-200 resize rounded-[20px] p-6 outline-none no-scroll"
+									className="bg-gray-200 resize rounded-[20px] h-[30vh] md:h-[40vh] p-6 outline-none no-scroll"
 								></textarea>
 							</div>
 
-							<button className="py-3 self-end px-16 rounded-[25px] text-[17px] bg-red-600 hover:bg-red-500 transition-all text-white font-medium">
+							<button className="py-2 md:py-3 self-end px-12 md:px-16 rounded-[25px] text-[17px] bg-red-600 hover:bg-red-500 transition-all text-white font-medium">
 								Send
 							</button>
 						</form>
 					</div>
 				) : (
-					<div className="mt-20 flex flex-col space-y-6 mx-auto max-w-[1020px]">
-						<h2 className="font-bold text-[25px]">Description</h2>
-						<p className="font-bold text-[28px] text-black/80">
+					<div className="mt-8 md:mt-20 flex flex-col space-y-3 mdspace-y-6 mx-auto max-w-[1020px]">
+						<h2 className="font-bold text-[20px] md:text-[25px]">
+							Description
+						</h2>
+						<p className="font-bold text-[22px] md:text-[28px] text-black/80">
 							Bedding set {product.text}
 						</p>
 						<p className="text-black/65">
@@ -204,9 +211,11 @@ export default function ProductDetails() {
 			</div>
 
 			<div className="flex flex-col items-center mx-auto max-w-[450px] xs:max-w-[600px] sm:max-w-[800px] md:max-w-[1460px]">
-				<h2 className="text-[38px] font-bold">Similar products</h2>
+				<h2 className="text-[28px] md:text-[38px] font-bold">
+					Similar products
+				</h2>
 
-				<div className="grid lg:grid-cols-4 gap-x-14 mt-16">
+				<div className="grid grid-cols-2 md:grid-cols-4 gap-y-10 gap-x-5 md:gap-x-14 mt-4 md:mt-16">
 					{product.similars.map((similar) => (
 						<Link
 							key={similar.id}
@@ -219,7 +228,7 @@ export default function ProductDetails() {
 								alt={similar.text}
 								className="rounded-[15px] w-[320px] hover:-translate-y-1 transition-all cursor-pointer"
 							/>
-							<p className="mt-4 text-[19px] tracking-wider text-sky-600 font-bold transition-all group-hover:text-red-600 group-hover:font-bold">
+							<p className="mt-4 xs:text-[19px] tracking-wider text-sky-600 font-bold transition-all group-hover:text-red-600 group-hover:font-bold">
 								{similar.text}
 							</p>
 						</Link>
